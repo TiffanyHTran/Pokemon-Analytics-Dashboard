eebb248a0cef1d1640c8c0cd7d18a30ce68356b9
tiffytran2000 <tiffytran2000@gmail.com>
2024-01-11 21:10:29 -0800

    update

--- data_warehouse.py
+++ data_warehouse.py
@@ -1,23 +1,27 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-#Extract data from pokemon.cvs
+# Extract data from pokemon.csv
 pokemon_data = pd.read_csv('pokemon.cvs')
 
-#Tranform data and create dimension tables
+# Transform data and create dimension tables
 dim_pokemon = pokemon_data[['ID', 'Name', 'Type1', 'Type2', 'Generation']].copy()
-dim_type = pd.DataFrame({'type_name': pokemon_data['Type1'].append(pokemon_data['Type2']).unique()})
+
+# Concatenate Type1 and Type2, then get unique values
+unique_types = pd.concat([pokemon_data['Type1'], pokemon_data['Type2']]).unique()
+dim_type = pd.DataFrame({'type_name': unique_types})
+
 dim_stat = pd.DataFrame({'stat_name': ['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']})
 dim_stat_value = pd.DataFrame({'value': pokemon_data[['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']].stack().unique()})
 
-#Load dimension tables into the data warehouse
+# Load dimension tables into the data warehouse
 engine = create_engine('sqlite:///pokemon_data_warehouse_complex.db')
 dim_pokemon.to_sql('dim_pokemon', engine, if_exists='replace', index=False)
 dim_type.to_sql('dim_type', engine, if_exists='replace', index=False)
 dim_stat.to_sql('dim_stat', engine, if_exists='replace', index=False)
 dim_stat_value.to_sql('dim_stat_value', engine, if_exists='replace', index=False)
 
-#Transform data and create fact table
+# Transform data and create fact table
 fact_pokemon_stat = pd.melt(pokemon_data, id_vars=['ID'], value_vars=['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'],
                             var_name='stat_name', value_name='value')
 fact_pokemon_stat = pd.merge(fact_pokemon_stat, dim_pokemon, left_on='ID', right_on='ID')
@@ -25,7 +29,7 @@ fact_pokemon_stat = pd.merge(fact_pokemon_stat, dim_type, left_on='Type1', right
 fact_pokemon_stat = pd.merge(fact_pokemon_stat, dim_stat, left_on='stat_name', right_on='stat_name')
 fact_pokemon_stat = pd.merge(fact_pokemon_stat, dim_stat_value, left_on='value', right_on='value')
 
-#Load the fact table to data warehouse
-fact_pokemon_stat[['ID', 'type_id', 'stat_id', 'stat_value_id', 'value']].to_sql('fact_pokemon_stat', engine, if_exists='replace', index=False)
+# Load the fact table into the data warehouse
+fact_pokemon_stat.to_sql('fact_pokemon_stat', engine, if_exists='replace', index=False)
 
 print("Data Warehousing Completed")
---
+++ real_time_processing.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from kafka import KafkaProducer
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+import plotly.express as px
+
